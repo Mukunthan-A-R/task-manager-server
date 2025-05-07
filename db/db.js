cf2c923fe87109f6cb52531531bf0be13b897bc9
Mukunthan A R <mukunthan.1255@gmail.com>
2025-05-07 13:12:32 +0530

    chainging db connection

--- db/db.js
+++ db/db.js
@@ -8,8 +8,6 @@ const { Pool } = require("pg");
 //   port: 5432,
 // });
 
-postgresql://done_it_61xv_user:tgP8MORf4WhWhNGDCFhUUmB0MuMVmXF2@dpg-d0dgd095pdvs7395905g-a.singapore-postgres.render.com/done_it_61xv
-
 // Render server
 const pool = new Pool({
   user: "done_it_61xv_user", // Replace with your PostgreSQL username
