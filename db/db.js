b360c6223a24b9e73585bae60211ae6f432957f6
Mukunthan A R <mukunthan.1255@gmail.com>
2025-06-06 15:01:56 +0530

    Configuring New neon db

--- db/db.js
+++ db/db.js
@@ -1,5 +1,18 @@
 const { Pool } = require("pg");
 
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "done it_owner",
+  host: "ep-muddy-salad-a1tuw1up-pooler.ap-southeast-1.aws.neon.tech",
+  database: "done it",
+  password: "npg_7aG2SefhEKor",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false, // Required for Neon
+  },
+});
+
 // const pool = new Pool({
 //   user: "postgres", // Replace with your PostgreSQL username
 //   host: "localhost",
@@ -9,13 +22,13 @@ const { Pool } = require("pg");
 // });
 
 // Render server
-const pool = new Pool({
-  user: "done_it_61xv_user", // Replace with your PostgreSQL username
-  host: "dpg-d0dgd095pdvs7395905g-a",
-  database: "done_it_61xv", // Replace with your database name
-  password: "tgP8MORf4WhWhNGDCFhUUmB0MuMVmXF2", // Replace with your PostgreSQL password
-  port: 5432,
-});
+// const pool = new Pool({
+//   user: "done_it_61xv_user", // Replace with your PostgreSQL username
+//   host: "dpg-d0dgd095pdvs7395905g-a",
+//   database: "done_it_61xv", // Replace with your database name
+//   password: "tgP8MORf4WhWhNGDCFhUUmB0MuMVmXF2", // Replace with your PostgreSQL password
+//   port: 5432,
+// });
 
 const connectDB = async () => {
   try {
